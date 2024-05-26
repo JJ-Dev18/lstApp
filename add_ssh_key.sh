8da66a7aac594a413b44ef2cae0accb71f98ff7c
Juan Murillo <jj.murillo182020@gmail.com>
2024-05-26 01:17:58 -0500

    modificando script sh [both] 6

--- add_ssh_key.sh
+++ add_ssh_key.sh
@@ -3,6 +3,5 @@
 # Iniciar el agente SSH
 eval "$(ssh-agent -s)"
 
-
 # Agregar la clave privada al agente SSH
-ssh-add <(echo "$SSH_PRIVATE_KEY") <<< "$SSH_PASSPHRASE"
+ssh-add <(echo "$SSH_PRIVATE_KEY")
