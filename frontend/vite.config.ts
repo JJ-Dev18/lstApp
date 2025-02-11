d277e0f5f2f13f119d64dc27caa3f4348505c70f
Juan <jj.murillo182020@gmail.com>
2025-02-11 12:23:25 -0500

    [both]

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import viteImagemin from 'vite-plugin-imagemin';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
