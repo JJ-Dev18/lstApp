796ddd561d5c8e32c6df7ca2e01bd21bc45eda9c
Juan <jj.murillo182020@gmail.com>
2025-02-26 13:18:20 -0500

    [frontend]

--- frontend/src/pages/public/components/Header.tsx
+++ frontend/src/pages/public/components/Header.tsx
@@ -16,11 +16,11 @@ const Header: React.FC = () => {
   return (
     <Box textAlign="center" position="relative" py={{ base: 0, md: 0, lg: 0 }} 
  
-    height={{ base: "400px", md: "600px", lg: "800px" , xl: '800px', '2xl' : '1100px'  }}
+    height={{ base: "400px", md: "400px", lg: "400px" , xl: '500px', '2xl' : '600px'  }}
     >
       <Skeleton position={"absolute"}  fadeDuration={1}   isLoaded={!loading} width="100%" height="100%" top={0}>
         <Box
-          bgImage="url(/assets/inicio/header2.webp)"
+          bgImage="url(/assets/inicio/header3.jpg)"
           bgPosition={{ base: "center" }}
           bgSize={{ base: "cover"  }}
           backgroundRepeat="no-repeat"
@@ -47,15 +47,12 @@ const Header: React.FC = () => {
       justifyContent="center"  alignItems="center"  
       height="100%"
       >
-        {/* <ChakraImage  src={LogoGrande} width={800}/> */}
-              <Heading as="h2" size={{ base: "2xl", md: "4xl" }} mb={4} color="white">
+       
+              <Heading as="h2" size={{ base: "2xl", md: "4xl" }} mb={4} color="white" display={{ base :'none', md : 'block'}}>
                 Ultimate Frisbee Tracker
               </Heading>
-              {/* <Text fontSize={{ base: "lg", md: "xl" }} mb={6} color="white">
-              ¿Estás listo para llevar tu torneo de ultimate frisbee al siguiente nivel? 
              
-              </Text> */}
-              <Text fontSize={{ base: "lg", md: "xl" }} mb={6} color="white">
+              <Text fontSize={{ base: "lg", md: "xl" }} mb={6} color="white"  display={{ base :'none' , md : 'block'}}>
               
               UFT es la herramienta para seguir tus partidos en tiempo real, estadísticas detalladas y análisis completo.
               </Text>
